6104021cb7f1ddd9137e68d32d2bad5b9792e004
IldikoMagda <ildiko.magda@gmail.com>
2023-09-11 12:09:17 +0100

    ready to use apis

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 const fruitForm = document.querySelector('#inputSection form')
+const fruitList = document.querySelector('#fruitSection ul')
 
-console.log("JS is working")
+const removeFruit = item => {
+    item.target.remove()
+}
+const addFruit = fruit => {
+    if (!fruit) {
+        return null
+    }
+    const li = document.createElement("li")
+    li.textContent = fruit
+    li.addEventListener("click", removeFruit, {once: true})
+    fruitList.appendChild(li)
+}
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault()
+    addFruit(e.target.fruitInput.value)
+    e.target.fruitInput.value = ""
+})
+
+console.log("fruitForm")
